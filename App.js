d2973628f8a7b0275356c61d7ac4372a428f6e60
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-04-26 21:58:20 -0600

    Accelerometer

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Accelerometer, Magnetometer} from 'expo-sensors';
+
+
+function SpeedySensor(){
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
+  const [subscription, setSubscription] = useState(null);
+  Accelerometer.setUpdateInterval(100)
+
+  const subscribe = () => {
+    setSubscription(
+      Accelerometer.addListener(accelerometerData => {
+        setData(accelerometerData);
+      })
+    );
+  };
+
+  const unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    subscribe();
+    return () => unsubscribe();
+  }, []);
+
+  const {x, y, z} = data;
+  return (
+    <View>{x}</View>
+  ); 
+}
+
+function MagnetoMeter(){
+  
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
